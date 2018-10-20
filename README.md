65f429bec4053d8d82e1c747706a45742946724b
Lu <ming.ml.lu@nokia.com>
2018-10-20 16:09:41 -0400

    Add scripts for video read, write and display

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # opencv-rw-video
 Read, write and display video using OpenCV
+
+See https://www.learnopencv.com/read-write-and-display-a-video-using-opencv-cpp-python/
--- read-display.py
+++ read-display.py
@@ -3,15 +3,15 @@ import cv2
 
 # Open a sample video available in sample-videos
 vcap = cv2.VideoCapture('http://108.53.114.166/mjpg/video.mjpg')
-#if not vcap.isOpened():
-#    print "File Cannot be Opened"
+if not vcap.isOpened():
+    print("File Cannot be Opened")
 while(True):
     # Capture frame-by-frame
     ret, frame = vcap.read()
     #print cap.isOpened(), ret
     if frame is not None:
         # Display the resulting frame
-        cv2.imshow('frame',frame)
+        cv2.imshow('Display live video ... Type q to quit',frame)
         # Press q to close the video windows before it ends if you want
         if cv2.waitKey(22) & 0xFF == ord('q'):
             break
