086fe4819422dbe5e265ca0d4fcf135ae728d4bd
Ming Lu <ming.ml.lu@nokia.com>
2020-06-26 16:05:30 -0400

    Update

--- read-video-frame.py
+++ read-video-frame.py
@@ -1,9 +1,34 @@
 import cv2
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--video_src", 
+                    type=str, 
+                    default = "", 
+                    help = "path to video file")
+parser.add_argument("--start_frame_no", 
+                    type=int, 
+                    default = 1, 
+                    help = "start frame number")
+parser.add_argument("--max_frames", 
+                    type=int, 
+                    default = -1, 
+                    help = "maximum number of frames to be read")
+parser.add_argument("--stride", 
+                    type=int, 
+                    default = 1, 
+                    help = "sampling interval")
+
+args = parser.parse_args()
+
+video_file = args.video_src
+
+stride = args.stride
 
 # Create a VideoCapture object
-cap = cv2.VideoCapture('fite_lab_demo.avi')
-cap.set(cv2.CAP_PROP_FPS, 15)
+cap = cv2.VideoCapture(video_file)
 
 # Check if camera opened successfully
 if (cap.isOpened() == False): 
@@ -16,20 +41,23 @@ frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
 # A counter for frames that have been written to the output file so far
-n_frames = 0
+n_frames = args.start_frame_no-1
 # The maximum number of frames to be written
-max_number_framed_to_be_read = 250
+max_number_framed_to_be_read = args.max_frames
 
 while(True):
   ret, frame = cap.read()
-  print(frame.shape)
 
   if ret == True: 
 
     n_frames += 1
+
+    if n_frames % stride != 0:
+      continue
+
     print("Frame %d out of %d read " % (n_frames, max_number_framed_to_be_read))
-    #if n_frames >= 60:
-    #  cv2.imwrite('frame_%03d.jpg'%(n_frames), frame)
+    if n_frames >= 0:
+      cv2.imwrite('frame_%05d.jpg'%(n_frames), frame)
     if n_frames == max_number_framed_to_be_read:
       break
     
@@ -37,10 +65,10 @@ while(True):
     cv2.imshow('Display live video while recording ... Type q to quit',frame)
  
     # Press Q on keyboard to stop recording
-    if cv2.waitKey() & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
       break
 
-  # Break the loop
+   #Break the loop
   else:
     break 
 
--- read_show_images.py
+++ read_show_images.py
@@ -1,16 +1,43 @@
 import os
 import re
 import cv2
+import argparse
 
-subtree_path = '.'
-ext_name = 'jpg'
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--path", 
+                    type=str, 
+                    default = ".", 
+                    help = "path to image files")
+parser.add_argument("--type", 
+                    type=str, 
+                    default = 'jpg', 
+                    help = "extension name of image files")
+parser.add_argument("--output_filename", 
+                    type=str, 
+                    default = 'video', 
+                    help = "output video file name")
+parser.add_argument("--fps", 
+                    type=float, 
+                    default = 1, 
+                    help = "frames per second")
+parser.add_argument("--check", 
+                    type=str, 
+                    default = 'No', 
+                    help = "check before saving: Yes or No")
+
+args = parser.parse_args()
+subtree_path = args.path
+ext_name = args.type
+check_before_saving = True if args.check!='No' else False
 
 def scan_image_files(subtree_path):
     list_images = []
     for (path, dirs, files) in os.walk(subtree_path):
         for file in files:
             if file.endswith(ext_name):
-                list_images.append(file)
+                list_images.append(os.path.join(path, file))
+    list_images.sort()
     return list_images
 
 list_images = scan_image_files(subtree_path)
@@ -18,12 +45,23 @@ list_images = scan_image_files(subtree_path)
 list_images_selected = []
 for fname in list_images:
     img = cv2.imread(fname)
-    cv2.imshow(fname, img)
-    #your_decison = cv2.waitKey()
-    #if your_decison == 1:
-    if cv2.waitKey() & 0xFF == ord('y'):
-        print(fname)
+    print("Image file name:", fname)
+    if check_before_saving:
+        cv2.imshow(fname, img)
+        if cv2.waitKey() & 0xFF == ord('y'):
+            list_images_selected.append(img)
+        elif cv2.waitKey() & 0xFF == ord('e'):
+            break
+        cv2.destroyAllWindows()
     else:
-        cv2.destroyWindow(fname)
-#cv2.destroyAllWindows()
+        list_images_selected.append(img)
+
+if list_images_selected:
+    H,W,_=list_images_selected[0].shape
+    # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+    fps = args.fps
+    output_filename = args.output_filename
+    out = cv2.VideoWriter(output_filename+'.avi',cv2.VideoWriter_fourcc('M','J','P','G'), fps, (W,H))
+    for img in list_images_selected:
+        out.write(img)
 
