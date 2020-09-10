db984961fc07acdac2055f4163696bcca361609b
Ming Lu <ming.ml.lu@nokia.com>
2020-09-10 10:45:33 -0400

    Update

--- read_show_images.py
+++ read_show_images.py
@@ -25,26 +25,44 @@ parser.add_argument("--check",
                     type=str, 
                     default = 'No', 
                     help = "check before saving: Yes or No")
+parser.add_argument("--repetitions", 
+                    type=int, 
+                    default = 1, 
+                    help = "times each images repaeted")
+parser.add_argument("--resolution", 
+                    type=str, 
+                    default = '720x1280', 
+                    help = "Video resolution, e.g., 720x1280")
 
 args = parser.parse_args()
 subtree_path = args.path
 ext_name = args.type
 check_before_saving = True if args.check!='No' else False
+resolution = args.resolution.split('x')
+if len(resolution) == 2:
+  H, W = resolution
+  H, W = int(H), int(W)
+else:
+  H, W = 0, 0
 
 def scan_image_files(subtree_path):
     list_images = []
     for (path, dirs, files) in os.walk(subtree_path):
         for file in files:
-            if file.endswith(ext_name):
+            if file.endswith(ext_name) and path == subtree_path:
                 list_images.append(os.path.join(path, file))
     list_images.sort()
     return list_images
 
 list_images = scan_image_files(subtree_path)
-
 list_images_selected = []
+if list_images:
+  if H == 0 or W == 0:
+    img = cv2.imread(list_images[0])
+    H, W, _ = img.shape
 for fname in list_images:
     img = cv2.imread(fname)
+    img = cv2.resize(img, (W,H), interpolation = cv2.INTER_AREA)
     print("Image file name:", fname)
     if check_before_saving:
         cv2.imshow(fname, img)
@@ -57,11 +75,13 @@ for fname in list_images:
         list_images_selected.append(img)
 
 if list_images_selected:
-    H,W,_=list_images_selected[0].shape
+    #H,W,_=list_images_selected[0].shape
     # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
     fps = args.fps
     output_filename = args.output_filename
     out = cv2.VideoWriter(output_filename+'.avi',cv2.VideoWriter_fourcc('M','J','P','G'), fps, (W,H))
     for img in list_images_selected:
         out.write(img)
+        for i in range(args.repetitions-1):
+            out.write(img)
 
