8ca299a8dd0cc5054a6fac630204ac096a20414a
Ming Lu <ming.ml.lu@nokia.com>
2020-10-14 23:23:25 -0400

    Resize factor

--- select_roi.py
+++ select_roi.py
@@ -8,10 +8,15 @@ parser.add_argument("--video_src",
                     type=str, 
                     default = "", 
                     help = "path to video file")
+parser.add_argument("--resize", 
+                    type=float, 
+                    default = 1, 
+                    help = "Downsize factor to get full view")
 
 args = parser.parse_args()
 
 video_file = args.video_src
+resize = args.resize
 
 # Create a VideoCapture object
 cap = cv2.VideoCapture(video_file)
@@ -25,9 +30,12 @@ frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 ret, frame = cap.read()
 if ret:
+    frame=cv2.resize(frame, (0,0), fx=resize, fy=resize)
     roi = cv2.selectROI(frame, False)
     print("Resolution: H %d, W %d" %(frame_height, frame_width))
     x, y, w, h = roi
+    x, y = int(x/resize),int(y/resize)
+    w, h = int(w/resize), int(h/resize)
     print("Selected ROI (x, y, w, h):", roi)
     print("Selected ROI (y1, x1, y2, x2):", (y, x, y + h, x + w))
 else:
